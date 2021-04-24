e03a71af9a29e1b24e983e7e41f2279fc9f16c31
Leszek B <l.buczek90@gmail.com>
2021-04-24 11:25:54 +0200

    security

---
+++ src/main/java/com/example/Warehouse/configuration/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.example.Warehouse.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
--- src/main/java/com/example/Warehouse/model1/dto/UsersDTO.java
+++ src/main/java/com/example/Warehouse/model1/dto/UsersDTO.java
@@ -3,6 +3,7 @@ package com.example.Warehouse.model1.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Bean;
 
 @Data
 @AllArgsConstructor
